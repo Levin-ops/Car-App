370f92cf190ccf7e67f87d585bd3a7259fc2a57a
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-11-09 08:06:31 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, {useState} from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import './App.css'
+import Search from "./Components/Search";
+import CarList from "./Components/CarList";
+
+function App() {
+  
+  const [searchTerm, setSearchTerm] = useState("")
+
+  // const components = [<Home />, <About />, <Recipe />];
+
+  return (
+    <Router>
+      <div>
+        <nav className="navbar">
+          <ul>
+            <li>
+              <Link to={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link to={"/about"}>About</Link>
+            </li>
+            <li>
+              <Link to={"/recipe"}>Car List</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* <hr /> */}
+
+        <Search onSearch={setSearchTerm} />
+
+        <Routes>
+        <Route exact path = "/" Component={Home} />
+        <Route exact path = "/about" Component={About} />
+        <Route exact path = "/recipe" Component={CarList} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Components/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../About.css'
+
+function About() {
+  return (
+    <div className='about-container'>
+      <h2 className='about-heading'>About The 5's Kitchen</h2>
+      <p className='about'>
+        Welcome to <em>The 5's Kitchen</em>, a culinary haven created by a team of passionate
+        food enthusiasts. Our mission is to share our love for food and our favorite
+        recipes with you. We believe that great food brings people together, and we're
+        excited to inspire your culinary journey.
+        
+        Our dedicated team is constantly exploring, experimenting, and curating the
+        finest recipes from around the world. From traditional classics to innovative
+        creations, we've got a diverse range of dishes for every palate. Join us in
+        celebrating the art of cooking and the joy of savoring delicious meals.
+      </p>
+    </div>
+  );
+}
+
+export default About;
